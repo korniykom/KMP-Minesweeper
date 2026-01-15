51d6a075715264e01170376ab9e49b68ad4e5a5b
Max <korniykom@gmail.com>
2026-01-15 10:57:37 +0200

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/korniykom/minesweeper/main.kt
@@ -0,0 +1,13 @@
+package com.korniykom.minesweeper
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Minesweeper",
+    ) {
+        App()
+    }
+}
