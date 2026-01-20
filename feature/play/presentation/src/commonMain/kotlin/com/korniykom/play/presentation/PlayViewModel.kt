ab2b38aac3ee465a22960e6fd2b2d6245c187700
Max <korniykom@gmail.com>
2026-01-20 19:55:04 +0200

    feat: add randomly generated board

--- feature/play/presentation/src/commonMain/kotlin/com/korniykom/play/presentation/PlayViewModel.kt
+++ feature/play/presentation/src/commonMain/kotlin/com/korniykom/play/presentation/PlayViewModel.kt
@@ -8,9 +8,9 @@ import com.korniykom.minesweeper.presentation.components.TileState
 import com.korniykom.settings.domain.BoardSettingsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class PlayViewModel(
     private val storage: Storage
@@ -20,18 +20,64 @@ class PlayViewModel(
 
     private val rowsFlow = storage.getAsFlow(BoardSettingsRepository.rowsKey);
     private val colsFlow = storage.getAsFlow(BoardSettingsRepository.colsKey)
+
     init {
         viewModelScope.launch {
             val rows = rowsFlow.firstOrNull() ?: 10
             val cols = colsFlow.firstOrNull() ?: 10
-            initBoard(rows, cols)
+            val numOfBombs = rows * cols / 10
+            initBoard(rows, cols, numOfBombs)
         }
     }
 
-    private fun initBoard(row: Int, col: Int) {
-        _boardState.value = List(row) {
-            List(col) {
-                TileState.Hidden(flagged = true)
+    private fun initBoard(row: Int, col: Int, numOfBombs: Int) {
+        val bombPositions = mutableSetOf<Pair<Int, Int>>()
+
+        while (bombPositions.size < numOfBombs) {
+            bombPositions += Random.nextInt(row) to Random.nextInt(col)
+        }
+        _boardState.value = List(row) { rowIndex ->
+            List(col) { columnIndex ->
+                if (rowIndex to columnIndex in bombPositions) {
+                    TileState.Revealed.Mine
+                } else {
+                    var number = 0
+                    if ((columnIndex - 1 to rowIndex + 1 in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex + 1 to rowIndex + 1 in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex to rowIndex + 1 in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex + 1 to rowIndex + 1 in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex - 1 to rowIndex in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex + 1 to rowIndex in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex - 1 to rowIndex - 1 in bombPositions)) {
+                        number++
+                    }
+                    if ((columnIndex to rowIndex - 1 in bombPositions)) {
+                        number++
+                    }
+                    when (number) {
+                        1 -> TileState.Revealed.Number(number)
+                        2 -> TileState.Revealed.Number(number)
+                        3 -> TileState.Revealed.Number(number)
+                        4 -> TileState.Revealed.Number(number)
+                        5 -> TileState.Revealed.Number(number)
+                        6 -> TileState.Revealed.Number(number)
+                        7 -> TileState.Revealed.Number(number)
+                        8 -> TileState.Revealed.Number(number)
+                        else -> TileState.Revealed.Number(null)
+                    }
+                }
             }
         }
     }
